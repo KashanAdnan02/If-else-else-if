2786c7ca1adb56a3654d439855f10680e350dcc0
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-02-09 01:44:45 -0800

    some-code

---
+++ Fahad/math/index.js
@@ -0,0 +1,16 @@
+// math methods
+
+// round, ceil, floor, random
+
+// ceiling 
+// floor 
+
+// var a = 6.9
+// console.log(Math.floor(a))
+
+// name.send()
+
+// var a = Math.random() * 10
+// var randomNumber = Math.round(a)
+
+// console.log(randomNumber)
