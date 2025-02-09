1f81a213e01ccbe5e87545db43ec9c781427eed4
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-02-09 01:36:24 -0800

    some-code

---
+++ Fahad/for loop/js/script.js
@@ -0,0 +1,92 @@
+// for loop is used to repeat work
+
+// 1 20000
+
+// for syntax (tareeka)
+// declaration condition increment
+// var a = 1
+// ++ -- + 1
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// table generator
+
+// var table = prompt("Enter any number to print table:")
+// for (var i = 1; i <= 10; i++) {
+//     document.write(table + " X " + i + " = " + table * i + "<br />")
+// }
+
+// document.write("<h1> Kashan </h1>")
+
+
+// let users = [
+//     {
+//         _id: "8823942734872394",
+//         _v: 0,
+//         username: "Kashan Adnan",
+//         password: "#$9n9h(*Y8u9q3heu23he9#",
+//         email: "kashan@gmail.com",
+//         number: 34993483423
+//     },
+//     {
+//         _id: "8823942734872394",
+//         _v: 0,
+//         username: "Kashan Adnan",
+//         password: "#$9n9h(*Y8u9q3heu23he9#",
+//         email: "kashan@gmail.com",
+//         number: 34993483423
+//     },
+//     {
+//         _id: "8823942734872394",
+//         _v: 0,
+//         username: "Kashan Adnan",
+//         password: "#$9n9h(*Y8u9q3heu23he9#",
+//         email: "kashan@gmail.com",
+//         number: 34993483423
+//     },
+//     {
+//         _id: "8823942734872394",
+//         _v: 0,
+//         username: "Kashan Adnan",
+//         password: "#$9n9h(*Y8u9q3heu23he9#",
+//         email: "kashan@gmail.com",
+//         number: 34993483423
+//     },
+//     {
+//         _id: "8823942734872394",
+//         _v: 0,
+//         username: "Kashan Adnan",
+//         password: "#$9n9h(*Y8u9q3heu23he9#",
+//         email: "kashan@gmail.com",
+//         number: 34993483423
+//     },
+//     {
+//         _id: "8823942734872394",
+//         _v: 0,
+//         username: "Kashan Adnan",
+//         password: "#$9n9h(*Y8u9q3heu23he9#",
+//         email: "kashan@gmail.com",
+//         number: 34993483423
+//     },
+//     {
+//         _id: "8823942734872394",
+//         _v: 0,
+//         username: "Kashan Adnan",
+//         password: "#$9n9h(*Y8u9q3heu23he9#",
+//         email: "kashan@gmail.com",
+//         number: 34993483423
+//     },
+//     {
+//         _id: "8823942734872394",
+//         _v: 0,
+//         username: "Fahad",
+//         password: "#$9n9h(*Y8u9q3heu23he9#",
+//         email: "fahad@gmail.com",
+//         number: 34993483423
+//     },
+// ]
+
+// for (let i = 0; i < users.length; i++) {
+//     console.log(users[i])
+// }
