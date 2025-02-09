7e9a4eaff0a846e1bd1f8d2cef4d13797c09119d
KashanAdnan02 <khanzaidaboy@gmail.com>
2025-02-09 01:15:47 -0800

    first commit

---
+++ Fahad/script.js
@@ -0,0 +1,42 @@
+// if (agar) conditions it is used to make conditions in javascript
+
+// keyword if
+// round bracket if(condition)
+// true false
+
+// {} block of code
+
+// conditional operators
+// == check if both of the value is same
+// === check if both of the value is same and also 
+// check the data type is same or not
+// < check if it is less than 
+// > check if it is greater than
+// <= if it is same or not
+// >= if it is same or not
+
+// &&, ||
+
+// and and, or or
+
+// && true true
+// || true false
+
+// var a = 1
+// var b = 2
+// // true
+// if (a == 2) {
+//     alert("Hello")
+// }
+// else if (a == 2) { // true
+//     alert("Else if")
+// }
+// else if (a == 3) {
+//     alert("Else if")
+// }
+// else {
+//     alert("hello 2")
+// }
+
+
+// if || else || else if
